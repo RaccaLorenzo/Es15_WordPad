988e49ecd04a67fa829ca696ad53a95380bddc78
l.racca.2295@vallauri.net <l.racca.2295@vallauri.net>
2022-12-02 11:50:20 +0100

    Aggiungere i file di progetto.

---
+++ frmWordPad.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Es15_WordPad
+{
+    public partial class frmWordPad : Form
+    {
+        public frmWordPad()
+        {
+            InitializeComponent();
+        }
+    }
+}
